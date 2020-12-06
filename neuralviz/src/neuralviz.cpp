c5af7d0b24fff45403f3c0ef5b7e0609cb33f437
dma <daniel.m.aldea@hotmail.com>
2020-12-06 21:28:21 +0100

    chore: created cmake project

---
+++ neuralviz/src/neuralviz.cpp
@@ -0,0 +1,12 @@
+﻿// neuralviz.cpp : Defines the entry point for the application.
+//
+
+#include "neuralviz.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
