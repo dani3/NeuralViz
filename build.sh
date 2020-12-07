2b18f256386ecc6005da2ce80bf97801da1649c8
dma <daniel.m.aldea@hotmail.com>
2020-12-07 22:02:00 +0100

    chore: added first version of build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "######################################"
+echo "#       Install dependencies         #"
+echo "######################################"
+
+# spdlog
+./vcpkg.exe install spdlog
+
+# build with cmake
+# TODO
