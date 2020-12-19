132f686ef0e54d81789f169628319aa027f9c729
dma <daniel.m.aldea@hotmail.com>
2020-12-19 23:19:43 +0100

    feat: implemented #1

--- src/Morpho/Neuron.cpp
+++ src/Morpho/Neuron.cpp
@@ -2,12 +2,49 @@
 
 namespace NeuralViz {
     Neuron::Neuron() {
-        mNodes = std::make_unique<std::vector<Node>>();
+        m_Nodes = std::make_shared<std::vector<Node>>();
     }
 
     Neuron::~Neuron() {}
 
     void Neuron::Push(const Node& node) {
-        mNodes->push_back(node);
+        m_Nodes->push_back(node);
+    }
+
+    NeuronIterator::NeuronIterator(const std::shared_ptr<std::vector<Node>> elements, int index)
+        : m_Elements(elements), m_Index(index) {}
+
+    NeuronIterator& NeuronIterator::operator++() {
+        m_Index++;
+        return *this;
+    }
+
+    NeuronIterator NeuronIterator::operator++(int) {
+        NeuronIterator iterator = NeuronIterator(m_Elements, m_Index);
+        m_Index++;
+        return iterator;
+    }
+
+    NeuronIterator& NeuronIterator::operator--() {
+        m_Index--;
+        return *this;
+    }
+
+    NeuronIterator NeuronIterator::operator--(int) {
+        NeuronIterator iterator = NeuronIterator(m_Elements, m_Index);
+        m_Index--;
+        return iterator;
+    }
+
+    const Node& NeuronIterator::operator*() const {
+        return m_Elements->at(m_Index);
+    }
+
+    bool NeuronIterator::operator!=(const NeuronIterator& other) const {
+        return this->m_Index != other.m_Index;
+    }
+
+    bool NeuronIterator::operator==(const NeuronIterator& other) const {
+        return this->m_Index == other.m_Index;
     }
 }
--- src/Morpho/Neuron.h
+++ src/Morpho/Neuron.h
@@ -4,14 +4,36 @@
 #include <Morpho/Node.h>
 
 namespace NeuralViz {
+    class NeuronIterator {
+    public:
+        NeuronIterator(const std::shared_ptr<std::vector<Node>> elements, int index);
+        const Node& operator*() const;
+        NeuronIterator& operator++();
+        NeuronIterator operator++(int);
+        NeuronIterator& operator--();
+        NeuronIterator operator--(int);
+        bool operator!=(const NeuronIterator& other) const;
+        bool operator==(const NeuronIterator& other) const;
+
+    private:
+        const std::shared_ptr<std::vector<Node>> m_Elements;
+        int m_Index = -1;
+    };
+
     class Neuron {
+    public:
+        using Iterator = NeuronIterator;
+
     public:
         Neuron();
         virtual ~Neuron();
 
         void Push(const Node& node);
 
+        Iterator begin() { return Iterator(m_Nodes, 0); }
+        Iterator end() { return Iterator(m_Nodes, m_Nodes->size()); }
+
     private:
-        std::unique_ptr<std::vector<Node>> mNodes;
+        std::shared_ptr<std::vector<Node>> m_Nodes;
     };
 }
